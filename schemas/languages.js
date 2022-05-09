a1e37f61f009131552d5488a441ccab46020548e
SamuelBassett <samabassett@gmail.com>
2022-05-09 15:01:25 +0200

    first commit

---
+++ schemas/languages.js
@@ -0,0 +1,8 @@
+// The languages you want to support.
+// They need 'id' and 'title'
+export const languages = [
+    { name: 'en_GB', title: 'English (UK)' },
+    { name: 'fr', title: 'French' },
+  ];
+  
+  export const baseLanguage = languages[0]
