5ece69b3d407c790dabcfbc8452a395e519c80e5
SamuelBassett <samabassett@gmail.com>
2022-05-10 10:26:46 +0200

    update

--- schemas/languages.js
+++ schemas/languages.js
@@ -1,8 +1,8 @@
 // The languages you want to support.
 // They need 'id' and 'title'
 export const languages = [
-    { name: 'en_GB', title: 'English (UK)' },
     { name: 'fr', title: 'French' },
+    { name: 'en_GB', title: 'English (UK)' },
   ];
   
   export const baseLanguage = languages[0]
