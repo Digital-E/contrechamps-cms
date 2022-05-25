c1c17aad3d5d6620f06bbf4b540cbb144f988d12
SamuelBassett <samabassett@gmail.com>
2022-05-25 11:40:50 +0200

    update

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,5 +1,6 @@
+
 const previewSecret = 'MY_SECRET' // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000'
+const projectUrl = process.env.SANITY_STUDIO_PROJECT_URL
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
